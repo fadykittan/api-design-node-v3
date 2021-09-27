389986fe91dfe60dda3b72b26f9a936f267986cc
Fady Kittan <fady.kittan@gmail.com>
2021-09-27 12:53:48 +0300

    Router and controllers

--- src/server.js
+++ src/server.js
@@ -12,4 +12,32 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const router = express.Router()
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'Okay' })
+})
+
+app.use('/api', router)
+
+// const log = (req, res, next) => {
+//   console.log('New request is coming')
+//   next()
+// }
+
+// app.use(log)
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'Ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log(`Server Started on port 3000`)
+  })
+}
