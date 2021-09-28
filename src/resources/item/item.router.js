1dff597b66bac679026629bf58497bb2dfa9b256
Fady Kittan <fady.kittan@gmail.com>
2021-09-28 19:05:49 +0300

    add item router

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,27 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  if (req.params.id) {
+    res.send({
+      id: req.params.id,
+      message: 'OK'
+    })
+  } else {
+    res.send({ message: 'OK' })
+  }
+}
+
 const router = Router()
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
