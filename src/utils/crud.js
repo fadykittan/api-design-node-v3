1b8ea7e1cc3023701e83d4a00ffabf3b2a246729
Fady Kittan <43303441+fadykittan@users.noreply.github.com>
2021-10-04 22:13:44 +0300

    add generic controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,67 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
 
-export const getMany = model => async (req, res) => {}
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
+  if (doc) {
+    return res.status(200).json({ data: doc })
+  } else {
+    return res.status(400).end()
+  }
+}
 
-export const createOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
 
-export const updateOne = model => async (req, res) => {}
+  const objectList = await model.find({ createdBy: userId }).exec()
+  if (objectList) {
+    return res.status(200).json({ data: objectList })
+  } else {
+    return res.status(404).end()
+  }
+}
 
-export const removeOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  const userId = req.user._id
+  const body = req.body
+
+  const newObj = await model.create({ createdBy: userId, ...body })
+
+  return res.status(201).json({ data: newObj })
+}
+
+export const updateOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const body = req.body
+
+  const updated = await model
+    .findOneAndUpdate({ _id: id, createdBy: userId }, body, {
+      new: true
+    })
+    .exec()
+
+  if (updated) {
+    res.status(200).json({ data: updated })
+  } else {
+    res.status(400).end()
+  }
+}
+
+export const removeOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  const removed = await model
+    .findOneAndRemove({ _id: id, createdBy: userId })
+    .exec()
+
+  if (removed) {
+    res.status(200).json({ data: removed })
+  } else {
+    res.status(400).end()
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
