46276bb86e3ea0f0ed42170aa840a847d7eed2d9
Fady Kittan <43303441+fadykittan@users.noreply.github.com>
2021-10-08 21:24:22 +0300

    complete auth js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,6 +2,8 @@ import config from '../config'
 import { User } from '../resources/user/user.model'
 import jwt from 'jsonwebtoken'
 
+const TOKEN_PREFIX = 'Bearer '
+
 export const newToken = user => {
   return jwt.sign({ id: user.id }, config.secrets.jwt, {
     expiresIn: config.secrets.jwtExp
@@ -16,10 +18,89 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (email && email.includes('@') && password && password.length > 6) {
+    const user = await User.create({ email: email, password: password })
+    const token = await newToken(user)
+    return res.status(201).send({ token: token })
+  } else {
+    return res.status(400).send({ message: 'requires email and password' })
+  }
+}
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  if (!email || !password) {
+    return res.status(400).send({ message: 'Your password or email is wrong' })
+  }
+
+  const user = await User.findOne({ email: email }).exec()
+
+  if (user) {
+    try {
+      const match = await user.checkPassword(password)
+
+      if (!match) {
+        return res
+          .status(401)
+          .send({ message: 'Your password or email is wrong' })
+      }
+    } catch (err) {
+      return res
+        .status(401)
+        .send({ message: 'Your password or email is wrong' })
+    }
+  } else {
+    return res.status(401).send({ message: 'Your password or email is wrong' })
+  }
+
+  const jwt = newToken(user)
+  return res.status(201).send({ token: jwt })
+}
+
+const getTokenFromHeaders = req => {
+  return req.headers.authorization
+}
+
+const isTokenVaild = token => {
+  if (token) {
+    return token.startsWith(TOKEN_PREFIX)
+  } else {
+    return false
+  }
+}
+
+const extractJwtFromToken = token => {
+  return token.substr(TOKEN_PREFIX.length)
+}
 
 export const protect = async (req, res, next) => {
+  const token = getTokenFromHeaders(req)
+
+  if (!isTokenVaild(token)) {
+    return res.status(401).end()
+  }
+
+  const jwt = await extractJwtFromToken(token)
+
+  try {
+    const userId = await verifyToken(jwt)
+    const user = await User.findById({ _id: userId.id })
+      .select('-password')
+      .lean()
+      .exec()
+    if (!user) {
+      return res.status(401).end()
+    } else {
+      req.user = user
+    }
+  } catch (e) {
+    console.error(e)
+    return res.status(401).end()
+  }
   next()
 }
